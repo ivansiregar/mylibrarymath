a2b3ab3374d1f350784ae08ba44489df569beed4
ivan siregar <ivan.siregar@gmail.com>
2021-05-18 10:23:26 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# mylibrarymath" 
+"# mylibrarymath" 
--- mylibrarymath/src/main/java/com/example/mylibrarymath/Calculate.kt
+++ mylibrarymath/src/main/java/com/example/mylibrarymath/Calculate.kt
@@ -8,6 +8,17 @@ class Calculate {
             return (i1 + i2)
         }
 
+        fun kurang (i1: Int, i2: Int) : Int
+        {
+            return (i1 - i2)
+        }
+
+        fun bagi (i1: Int, i2: Int) : Int
+        {
+            require (i2 != 0) {"Devide by zero, error!"}
+            return (i1 / i2)
+        }
+
         fun kali (i1: Int, i2: Int) : Int
         {
             return (i1 * i2)
